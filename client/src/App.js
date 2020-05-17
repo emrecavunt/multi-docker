e01eb6666648ddc464d2488837d260ee57a4b6de
emre <emre.cavunt@gmail.com>
2020-05-17 19:51:19 +0300

    initial

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/">Home </Link>
+        <Link to="/otherpage">Other Page</Link>
+      </div>
+      <Route exact path="/" component={Fib} />
+      <Route path="/otherpage" component={OtherPage} />
+    </Router>
+  );
+}
+
+export default App;
